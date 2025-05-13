993416c458bd48d58fdbab547778728e182c12ce
Armstrong-Capital-Financial-Services <divyanshu@armstrong-cap.com>
2025-05-13 15:38:46 +0530

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,16 @@
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import CandidateAssessment from './pages/CandidateAssessment';
+import AdminDashboard from './pages/AdminDashboard';
 
+function App() {
+  return (
+    <Routes>
+      <Route path="/assessment" element={<CandidateAssessment />} />
+      <Route path="/admin" element={<AdminDashboard />} />
+      <Route path="/" element={<Navigate to="/assessment" replace />} />
+    </Routes>
+  );
+}
+
+export default App;
